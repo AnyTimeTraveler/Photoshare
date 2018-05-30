3864ec6a4c3f9c03017b9c26395af08b7bb5c9b5
Simon Struck <simonstruck70@gmail.com>
2018-05-31 00:59:06 +0200

    Replace bootstrap with bluma

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,40 +1,48 @@
-import React, { Component } from 'react';
-import { ApolloClient } from 'apollo-client';
+import React from 'react';
+import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import { HttpLink } from 'apollo-link-http';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import MyPhotos from './pages/myPhotos';
 import TopBar from './components/TopBar';
 import Settings from './pages/settings';
 import SearchResults from './pages/searchResults';
+import { createStore } from 'redux';
+
 import Upload from './pages/upload';
 import Login from './pages/login';
 import ProtectedRoute from './components/ProtectedRoute';
+import reducers from './state/reducers';
+import { AppContainer } from 'react-hot-loader';
+
 
 const client = new ApolloClient({
-    link: new HttpLink({
-        uri: `http://localhost:8080/graphql/`,
-    }),
-    cache: new InMemoryCache(),
+    uri: 'http://localhost:8080/graphql',
 });
 
-class App extends Component {
+
+const store = createStore(
+    reducers,
+    {},
+);
+
+class App extends React.Component {
     render() {
-        return <ApolloProvider client={client}>
-            <BrowserRouter>
-                <div>
-                    <TopBar/>
-                    <Switch>
-                        <Route path="/login" component={Login}/>
-                        <ProtectedRoute path="/" exact={true} component={MyPhotos}/>
-                        <ProtectedRoute path="/upload" component={Upload}/>
-                        <ProtectedRoute path="/search" component={SearchResults}/>
-                        <ProtectedRoute path="/settings" component={Settings}/>
-                    </Switch>
-                </div>
-            </BrowserRouter>
-        </ApolloProvider>;
+        return <AppContainer>
+            <ApolloProvider client={client} store={store}>
+                <BrowserRouter>
+                    <div>
+                        <TopBar/>
+                        <Switch>
+                            <Route path="/login" component={Login}/>
+                            <ProtectedRoute path="/" exact={true} component={MyPhotos}/>
+                            <ProtectedRoute path="/upload" component={Upload}/>
+                            <ProtectedRoute path="/search" component={SearchResults}/>
+                            <ProtectedRoute path="/settings" component={Settings}/>
+                        </Switch>
+                    </div>
+                </BrowserRouter>
+            </ApolloProvider>
+        </AppContainer>;
     }
 }
 
