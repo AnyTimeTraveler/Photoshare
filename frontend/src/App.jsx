1fd4fb18985ebc6600ab81ea1afd2fec343f9cec
Simon Struck <simon@simonscode.org>
2018-08-02 23:41:23 +0200

    Add landing page and remove ApolloProvider

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { ApolloProvider } from 'react-apollo';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { AppContainer } from 'react-hot-loader';
 import { createStore } from 'redux';
@@ -8,12 +7,12 @@ import MyPhotos from './pages/myPhotos';
 import NavBar from './components/presentational/navbar/NavBar';
 import Settings from './pages/settings';
 import SearchResults from './pages/searchResults';
+import Welcome from './pages/welcome';
 
 import Upload from './pages/upload';
 import Login from './pages/login';
 import ProtectedRoute from './components/ProtectedRoute';
 import reducers from './state/reducers';
-import apolloClient from './ApolloClient';
 
 /* eslint-disable no-underscore-dangle */
 // noinspection JSUnresolvedVariable
@@ -26,22 +25,21 @@ const store = createStore(
 class App extends React.Component {
     render() {
         return <AppContainer>
-            <ApolloProvider client={apolloClient}>
-                <Provider store={store}>
-                    <BrowserRouter>
-                        <div>
-                            <NavBar/>
-                            <Switch>
-                                <Route path={'/login'} component={Login}/>
-                                <ProtectedRoute path={'/'} exact={true} component={MyPhotos}/>
-                                <ProtectedRoute path={'/upload'} component={Upload}/>
-                                <ProtectedRoute path={'/search'} component={SearchResults}/>
-                                <ProtectedRoute path={'/settings'} component={Settings}/>
-                            </Switch>
-                        </div>
-                    </BrowserRouter>
-                </Provider>
-            </ApolloProvider>
+            <Provider store={store}>
+                <BrowserRouter>
+                    <div>
+                        <NavBar/>
+                        <Switch>
+                            <Route path={'/login'} component={Login}/>
+                            <Route path={'/'} exact={true} component={Welcome}/>
+                            <ProtectedRoute path={'/myphotos'} component={MyPhotos}/>
+                            <ProtectedRoute path={'/upload'} component={Upload}/>
+                            <ProtectedRoute path={'/search'} component={SearchResults}/>
+                            <ProtectedRoute path={'/settings'} component={Settings}/>
+                        </Switch>
+                    </div>
+                </BrowserRouter>
+            </Provider>
         </AppContainer>;
     }
 }
---
+++ frontend/src/pages/welcome/index.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+import Box from '../../components/presentational/forms/Box';
+
+
+export default class Upload extends Component {
+    render() {
+        return <Box style={{ margin: '5%' }}>
+            <h1>Landing page!</h1>
+        </Box>;
+    }
+}
